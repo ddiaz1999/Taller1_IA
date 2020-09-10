29b71321e6be31d406ad5410acb89df7554f438d
JhonHader <45322492+JhonHader@users.noreply.github.com>
2020-09-10 15:06:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
 import os
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import data as df
+
+def get_Normalize(dataframe):
+    x = dataframe.values  # returns a numpy array
+    min_max_scaler = MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    x = pd.DataFrame(x_scaled)
+    return x
 
 if __name__ == "__main__":
 
     # Read data from csv file
-    path = 'C:/Users/lenovo/Desktop/JHON_2030/IA/TALLER_1/dataset/'
-    filename = 'training.csv'
-    file = os.path.join(path, filename)
-    train_data = pd.read_csv(file, na_values=' ?')
+    train_data_path = 'C:/Users/lenovo/Desktop/JHON_2030/IA/TALLER_1/dataset/'
+    train_data_filename = 'training.csv'
+    train_data_file = os.path.join(train_data_path, train_data_filename)
 
-    # See total data
-    original_lenght_data = train_data.shape[0]
-    train_data = train_data.dropna()
-    print('\nOriginal lenght of data:', original_lenght_data)
-    print('After remove miss data lenght:', train_data.shape[0])
-    print('Total miss values:', original_lenght_data-train_data.shape[0], '\n')
-
-    # See data balance
-    '''
-    total_data = train_data.shape[0]
-    data_balance = np.array(train_data['income'].value_counts())
-    percent_incomes_more_than_50k = data_balance[1] * 100 / total_data
-    percent_incomes_less_than_50k = 100 - percent_incomes_more_than_50k
-    print('\nData balance')
-    print('>50', round(percent_incomes_more_than_50k, 2))
-    print('<=50', round(percent_incomes_less_than_50k, 2), '\n')
-    '''
-
-    # Characteristics
-    characteristics = train_data.columns[:-1]
-
-    # Cualitative data integer codification
-    #train_data['Sex'].str.strip().replace(['Female', 'Male'], [0, 1], inplace=True)
-    #train_data['Race'].str.strip().replace(['White', 'Black', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other'], [0, 1, 2, 3, 4], inplace=True)
-    #train_data['Workclass'].str.strip().replace(['Private', 'Self-emp-not-inc', 'Local-gov', 'State-gov', 'Self-emp-inc', 'Federal-gov', 'Without-pay'], [0, 1, 2, 3, 4, 5, 6], inplace=True)
-
-    Integer_coding = {}
-    for col_name in train_data.columns:
-        if (train_data[col_name].dtype == 'object'):
-            x = train_data[col_name].value_counts().keys().tolist()
-            x.sort()
-            Integer_coding[col_name] = x
-            train_data[col_name] = train_data[col_name].astype('category')
-            train_data[col_name] = train_data[col_name].cat.codes
-
-    X_train, Y_train = train_data.drop(train_data.columns[-1], axis=1), train_data.drop(characteristics, axis=1)
-    print(X_train.shape, Y_train.shape)
+    data_train = df.dataframe(train_data_file) # cargamos el dataframe
+    #data_train.get_Data_Info()
+    #data_train.get_data_balance()
+    data_train.One_Hot_Encoding()
+    new_charactertistics = data_train.get_characteristics()
+    X, Y_train = data_train.get_Split_Data()
+    X_train = get_Normalize(X)
+    #print(X['Workclass_ Private'])
+    #print(X_train[new_charactertistics.index('Workclass_ Private')])
